8d2d1fb3e9f573d429fc783ab60e78ebad6bc53c
StarShadow <oparkerj@gmail.com>
2019-06-03 15:47:19 -0700

    Delete files from previous version

--- src/com/ssplugins/shadow/common/ClassFinder.java
+++
@@ -1,9 +0,0 @@
-package com.ssplugins.shadow.common;
-
-import java.util.Optional;
-
-public interface ClassFinder {
-	
-	Optional<Class<?>> findClass(String input);
-	
-}
--- src/com/ssplugins/shadow/common/Parser.java
+++
@@ -1,7 +0,0 @@
-package com.ssplugins.shadow.common;
-
-public interface Parser {
-	
-	String parse(String line);
-	
-}
